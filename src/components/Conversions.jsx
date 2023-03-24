4453e5c6cd36de9aa3331254f2b93795741c4bf0
justinZ45 <justinzaluk@gmail.com>
2023-03-24 11:46:52 -0400

    prettier and eslint changes

--- src/components/Conversions.jsx
+++ src/components/Conversions.jsx
@@ -1,117 +1,115 @@
-import axios from "axios";
-import { useState, useEffect } from "react";
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 const Conversions = () => {
-  const [rateData, setRateData] = useState([]);
-  const [usdRate, setUsdRate] = useState(0);
-  const [gbpRate, setGbpRate] = useState(0);
-  const [eurRate, setEurRate] = useState(0);
-  const [currencyChoice, setCurrencyChoice] = useState("");
-  const [search, setSearch] = useState("");
-  const [convertedValue, setConvertedValue] = useState(0);
-  const [dataDate, setDataDate] = useState("");
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+	const [rateData, setRateData] = useState([]);
+	const [usdRate, setUsdRate] = useState(0);
+	const [gbpRate, setGbpRate] = useState(0);
+	const [eurRate, setEurRate] = useState(0);
+	const [currencyChoice, setCurrencyChoice] = useState('');
+	const [search, setSearch] = useState('');
+	const [convertedValue, setConvertedValue] = useState(0);
+	const [dataDate, setDataDate] = useState('');
+	const [loading, setLoading] = useState(false);
+	const [error, setError] = useState(null);
 
-  const currencyChoiceHandler = (e) => {
-    //function to set user's choice of currency
-    setCurrencyChoice(e.target.value);
-  };
+	const currencyChoiceHandler = (e) => {
+		//function to set user's choice of currency
+		setCurrencyChoice(e.target.value);
+	};
 
-  const searchHandler = (e) => {
-    //function to set user's input
-    setSearch(parseInt(e.target.value));
-    if (isNaN(search)) {
-      //prevent NAN error
-      setSearch(0);
-    }
-  };
+	const searchHandler = (e) => {
+		//function to set user's input
+		setSearch(parseInt(e.target.value));
+		if (isNaN(search)) {
+			//prevent NAN error
+			setSearch(0);
+		}
+	};
 
-  const convert = () => {
-    //check user's choice and perform calculations to convert to BTC
-    if (currencyChoice == "USD") {
-      setConvertedValue((1 / usdRate.rate_float).toFixed(6) * search);
-    } else if (currencyChoice == "GBP") {
-      setConvertedValue((1 / gbpRate.rate_float).toFixed(6) * search);
-    } else {
-      setConvertedValue((1 / eurRate.rate_float).toFixed(6) * search);
-    }
-  };
+	const convert = () => {
+		//check user's choice and perform calculations to convert to BTC
+		if (currencyChoice == 'USD') {
+			setConvertedValue((1 / usdRate.rate_float).toFixed(6) * search);
+		} else if (currencyChoice == 'GBP') {
+			setConvertedValue((1 / gbpRate.rate_float).toFixed(6) * search);
+		} else {
+			setConvertedValue((1 / eurRate.rate_float).toFixed(6) * search);
+		}
+	};
 
-  const fetchRates = async () => {
-    //function to obtain data from API
-    setLoading(true);
-    setError(null);
-    const rateUrl = "https://api.coindesk.com/v1/bpi/currentprice.json";
+	const fetchRates = async () => {
+		//function to obtain data from API
+		setLoading(true);
+		setError(null);
+		const rateUrl = 'https://api.coindesk.com/v1/bpi/currentprice.json';
 
-    await axios
-      .get(rateUrl)
-      .then(({ data }) => {
-        //set all data from API
-        setRateData(data);
-        setUsdRate(data.bpi.USD);
-        setGbpRate(data.bpi.GBP);
-        setEurRate(data.bpi.EUR);
-        setDataDate(data.time.updatedISO);
-      })
-      .catch((error) => {
-        setError(error);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  };
+		await axios
+			.get(rateUrl)
+			.then(({ data }) => {
+				//set all data from API
+				setRateData(data);
+				setUsdRate(data.bpi.USD);
+				setGbpRate(data.bpi.GBP);
+				setEurRate(data.bpi.EUR);
+				setDataDate(data.time.updatedISO);
+			})
+			.catch((error) => {
+				setError(error);
+			})
+			.finally(() => {
+				setLoading(false);
+			});
+	};
 
-  useEffect(() => {
-    //fetchrates when component is loaded
-    fetchRates();
-  }, []);
+	useEffect(() => {
+		//fetchrates when component is loaded
+		fetchRates();
+	}, []);
 
-  const curDate = new Date(dataDate); //data object used to convert UTC to local time
+	const curDate = new Date(dataDate); //data object used to convert UTC to local time
 
-  return (
-    <div>
-      {!!error && <pre>{JSON.stringify(error, 0, 1)}</pre>}
-      {!!loading && <p>Loading</p>}
+	return (
+		<div>
+			{!!error && <pre>{JSON.stringify(error, 0, 1)}</pre>}
+			{!!loading && <p>Loading</p>}
 
-      {rateData !== null ? ( //check if data is null using ternary operator
-        <div>
-          <button onClick={fetchRates} className="button">
-            {" "}
-            Refresh Rates
-          </button>
-          <p>Data Date (Local): {curDate.toString()}</p>
-          <p>Data Date (UTC): {curDate.toUTCString()}</p>
-          <select
-            className="form-select form-select-sm mb-3"
-            value={currencyChoice}
-            onChange={currencyChoiceHandler}
-          >
-            <option defaultValue={"Select a Currency"}>
-              Select a Currency
-            </option>
-            <option>USD </option>
-            <option>GBP</option>
-            <option>EUR</option>
-          </select>
+			{rateData !== null ? ( //check if data is null using ternary operator
+				<div>
+					<button onClick={fetchRates} className="button">
+						{' '}
+						Refresh Rates
+					</button>
+					<p>Data Date (Local): {curDate.toString()}</p>
+					<p>Data Date (UTC): {curDate.toUTCString()}</p>
+					<select
+						className="form-select form-select-sm mb-3"
+						value={currencyChoice}
+						onChange={currencyChoiceHandler}
+					>
+						<option defaultValue={'Select a Currency'}>Select a Currency</option>
+						<option>USD </option>
+						<option>GBP</option>
+						<option>EUR</option>
+					</select>
 
-          <label htmlFor="search" className="p">
-            Enter a Currency Value:{" "}
-          </label>
-          <input //input for currency value entered by user
-            type="text"
-            name="search"
-            onChange={searchHandler}
-            value={search}
-          />
-          <button onClick={convert} className="button">
-            Submit
-          </button>
-          <p>Converted Value is: {convertedValue} BTC</p>
-        </div>
-      ) : null}
-    </div>
-  );
+					<label htmlFor="search" className="p">
+						Enter a Currency Value:{' '}
+					</label>
+					<input //input for currency value entered by user
+						type="text"
+						name="search"
+						onChange={searchHandler}
+						value={search}
+					/>
+					<button onClick={convert} className="button">
+						Submit
+					</button>
+					<p>Converted Value is: {convertedValue} BTC</p>
+				</div>
+			) : null}
+		</div>
+	);
 };
 
 export default Conversions;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,21 @@
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 const NavBar = () => {
-  return (
-    <>
-      <Navbar bg="dark" variant="dark" fixed="top">
-        <Container>
-          <Navbar.Brand>Project 1</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="/">Current Rates</Nav.Link>
-            <Nav.Link href="/conversions">Conversions</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-    </>
-  );
+	return (
+		<>
+			<Navbar bg="dark" variant="dark" fixed="top">
+				<Container>
+					<Navbar.Brand>Project 1</Navbar.Brand>
+					<Nav className="me-auto">
+						<Nav.Link href="/">Current Rates</Nav.Link>
+						<Nav.Link href="/conversions">Conversions</Nav.Link>
+					</Nav>
+				</Container>
+			</Navbar>
+		</>
+	);
 };
 
 export default NavBar;
